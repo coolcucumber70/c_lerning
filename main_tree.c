5e98f2e7606a602987f35258138b5cfe5f786ae6
coolcucumber70 <794746725@qq.com>
2021-10-24 15:30:30 +0800

    2021.10.24

--- main_tree.c
+++ main_tree.c
@@ -14,11 +14,48 @@ int main()
     // postorderBST(shead);
     BTNode*head=NULL;
     createtree(&head,4);
-    preorder(head);
-    printf("\n");
-    inorder(head);
-    printf("\n");
-    postorder(head);
-    printf("\n");
+    // preorder(head);
+    // printf("\n");
+    // inorder(head);
+    // printf("\n");
+    // postorder(head);
+    // printf("\n");
+
     return 0;
 }
+ int hightbylecvel(BTNode* head)
+ {
+     int hight=0,front=0,rear=0;
+     BTNode* queue[MaxSize];
+     if(!head)
+     {
+         return 0;
+     }
+     rear=(rear+1)%MaxSize;
+     queue[rear]=head;
+     BTNode*q=queue[rear];
+     while(front!=rear)
+     {
+         front=(front+1)%MaxSize;
+     
+     BTNode* p=queue[front];
+
+     if(p->lchild)
+     {
+         rear=(rear+1)%MaxSize;
+         queue[rear]=p->lchild;
+     }
+     if(p->rchild)
+     {
+         rear=(rear+1)%MaxSize;
+         queue[rear]=p->rchild;
+     }
+     if(p==q)
+     {
+         hight++;
+         q=queue[rear];
+     }
+
+     }
+     return hight;
+ }
