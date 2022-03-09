523455557268bf29944c22824256c61265e24db7
coolcucumber70 <794746725@qq.com>
2022-03-09 22:26:41 +0800

    20223.9

--- c_test/c_pro/string_2.c
+++ c_test/c_pro/string_2.c
@@ -3,25 +3,27 @@
 
 void fun(char *s, char *t, int* k)
 {
-    int i;
-    *k = 0;
-    for (i = 0; s[i]!='\0'; i++)
-        if ('0' <= s[i] && s[i] <= '9')
-        {
-            t[*k] = s[i];
-            *k += 1;
-        }
-        t[*k]='\0';
+	int i;
+	*k = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ('0' <= s[i] && s[i] <= '9')
+		{
+			t[*k] = s[i];
+			*k += 1;
+		}
+	}
+
 }
 void main()
 {
-    char str[100], tOrigin[9];
-    long int n;
-    int i, k;
-    printf("Please enter string s:");
-    scanf("%s", str);
-    fun(str, tOrigin, &k);
-    for (i = 0; i < k; i++)
-        n += (tOrigin [k - 1 - i] - '0') * pow(10, i);
-    printf("The result is: %d", n);
+	char str[100], tOrigin[9];
+	long long n=0;
+	int i, k;
+	printf("Please enter string s:");
+	scanf("%s", str);
+	fun(str, tOrigin, &k);
+	for (i = 0; i < k; i++)
+	n += (long long)(tOrigin[k - 1 - i] - '0') * pow(10, i);
+	printf("The result is: %ld", n);
 }
